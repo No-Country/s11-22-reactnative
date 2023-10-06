7048dd9bc0f5a0c9068c73a681a19d95288eac8e
Josue O. Monge <razuordonez@gmail.com>
2023-10-06 18:11:34 -0500

    init

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-teal-500">
+        Open up App.js to start working on your app!
+      </Text>
+    </View>
+  )
+}
+
+export default HomeScreen
