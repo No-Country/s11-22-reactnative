b577de945ec19f5e15b6ec593dd9f360fa3489eb
Diego-Fdez <dieghoxcr7@gmail.com>
2023-10-09 20:02:11 -0600

    update barrel

--- app/screens/index.js
+++ app/screens/index.js
@@ -1,1 +1,2 @@
-export { default as LoginScreen } from './login/LoginScreen';
+export { default as HomeScreen } from './home/HomeScreen'
+export { default as LoginScreen } from './login/LoginScreen'
