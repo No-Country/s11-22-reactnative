4e946a73618fdffb3fc6716fccb80ead9d80d5a4
Diego-Fdez <dieghoxcr7@gmail.com>
2023-10-10 21:18:31 -0600

    fix styles

--- app/components/searchBox/SearchBox.jsx
+++ app/components/searchBox/SearchBox.jsx
@@ -1,14 +1,14 @@
 import { TextInput } from 'react-native'
 import normalize from 'react-native-normalize'
 
-const SearchBox = ({ width }) => {
+const SearchBox = () => {
   return (
     <TextInput
-      className="px-3 h-11 rounded-lg bg-[#D9D9D9] text-black font-light leading-5 tracking-tight"
+      className="px-3 h-11 rounded-lg bg-[#D9D9D9] text-black font-light leading-5 tracking-tight w-full"
       placeholder="Search"
       placeholderTextColor="#000"
       keyboardType="web-search"
-      style={{ fontSize: normalize(16), width: normalize(width) }}
+      style={{ fontSize: normalize(16) }}
     />
   )
 }
