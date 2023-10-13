0f2de10523cb1e83ebeb64258a33a481feb8fe81
Diego-Fdez <dieghoxcr7@gmail.com>
2023-10-13 14:57:28 -0600

    move component to search screen

--- app/screens/searchScreen/components/SearchBox.jsx
+++ app/screens/searchScreen/components/SearchBox.jsx
@@ -4,11 +4,11 @@ import normalize from 'react-native-normalize'
 const SearchBox = () => {
   return (
     <TextInput
-      className="px-3 h-11 rounded-lg bg-[#D9D9D9] text-black font-light leading-5 tracking-tight w-full"
+      className="px-3 h-11 rounded-lg bg-[#D9D9D9] text-black font-light leading-5 tracking-tight"
       placeholder="Search"
       placeholderTextColor="#000"
       keyboardType="web-search"
-      style={{ fontSize: normalize(16) }}
+      style={{ fontSize: normalize(16), width: normalize(309) }}
     />
   )
 }
