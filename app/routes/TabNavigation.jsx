d53f6938dfe54db40214f94039c3895cb701a5da
Diego-Fdez <dieghoxcr7@gmail.com>
2023-10-17 17:11:53 -0600

    fix styles

--- app/routes/TabNavigation.jsx
+++ app/routes/TabNavigation.jsx
@@ -1,18 +1,37 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Dimensions } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import normalize from 'react-native-normalize'
+import Svg, { Path } from 'react-native-svg'
 import { CartScreen, HomeScreen } from '../screens'
 
 const Tab = createBottomTabNavigator()
 
 const TabNavigation = () => {
+  const { width } = Dimensions.get('window')
+
   return (
     <Tab.Navigator
       screenOptions={{
         tabBarStyle: {
           ...styles.container,
         },
+        tabBarBackground: () => (
+          <Svg
+            width={`${width}`}
+            height={`${normalize(62)}`}
+            viewBox="0 0 393 63"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <Path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M153.57 10.7578C146.932 5.23424 139.113 0.59436 130.477 0.59436H1C0.447715 0.59436 0 1.04207 0 1.59436V62C0 62.5523 0.447713 63 0.999997 63H392C392.552 63 393 62.5523 393 62V1.59436C393 1.04208 392.552 0.59436 392 0.59436H261.596C252.96 0.59436 245.141 5.23424 238.503 10.7578C227.816 19.6513 212.741 25.1925 196.037 25.1925C179.332 25.1925 164.258 19.6513 153.57 10.7578Z"
+              fill="#CFD1C0"
+            />
+          </Svg>
+        ),
       }}
     >
       <Tab.Screen
@@ -67,9 +86,12 @@ export default TabNavigation
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#CFD1C0',
+    position: 'absolute',
     height: normalize(62),
     paddingBottom: normalize(10, 'height'),
+    shadowColor: 'transparent',
+    backgroundColor: 'transparent',
+    borderTopColor: 'transparent',
   },
   text: {
     fontFamily: 'mrt-400',
@@ -81,7 +103,7 @@ const styles = StyleSheet.create({
     width: normalize(55),
     height: normalize(55),
     borderRadius: 55 / 2,
-    marginTop: normalize(-60, 'height'),
+    marginTop: normalize(-70, 'height'),
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
